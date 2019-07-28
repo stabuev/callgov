821d8be051e99888bb0250d9437f528baf0dc2f6
stabuev <soslan.tabuev@gmail.com>
2019-07-28 09:39:49 +0300

    summary

--- front/cdn/script.js
+++ front/cdn/script.js
@@ -86,3 +86,45 @@ function getObrList(){
     }    
     req.send();       
 }
+
+function getObr(id){
+    var content = document.getElementById("obrList")
+	while (content.firstChild) {
+		content.removeChild(content.firstChild);
+	}
+	var req = new XMLHttpRequest();
+    req.open("POST", "/json/obrlist");
+    req.setRequestHeader("Content-Type", "application/json");
+    req.send('{"id":' + id + '"}'); 
+	req.onload = function (e) {
+            var msg = JSON.parse(req.responseText);
+            console.log(id);
+            for(var obr of msg.obr){
+                console.log(obr)
+                var div=document.createElement("div");
+                div.classList.add("card", "m-3")
+                div.innerHTML = 
+            '<div class="row no-gutters">' +
+              '<div class="col-md-12">' +
+                '<div class="card-body">' +
+                  '<a href="/detail?id=' + obr[0] +'"><h5 class="card-title">' + obr[1] + '</h5></a>' +
+                  '<p class="card-text">' + obr[2].slice(0, 250) + '... </p>' +
+                  '<div class="row no-gutters">' +
+                      '<div class="col-md-6">' +
+                  '<p class="card-text">' +
+                      '<small>Автор: <span class="text-muted">' + obr[3]+ '</span></small><br />' +
+                    '<small>Адресат: <span class="text-muted">'+ obr[6] + '</span></small></div>' +
+                    '<div class="col-md-6">' +
+                        '<p class="card-text">' +
+   '<small>Подписей: <span class="text-muted">238</span></small></br>' +
+                  '<small>Комментариев: <span class="text-muted">42</span></small></p>' +
+                  '</div></div>' +
+                '</div>' +
+              '</div>' +
+            '</div>'
+            content.appendChild(div);
+
+                
+            }
+    }          
+}
