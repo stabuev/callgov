c35956d3ff085f1dd66182527bb653bf85533654
stabuev <soslan.tabuev@gmail.com>
2019-07-28 05:07:06 +0300

    commit zhe est

--- front/cdn/script.js
+++ front/cdn/script.js
@@ -1,6 +1,4 @@
 function doLogin() {
-	recLoginUsed = true;
-
 	var login = document.getElementById("loginName").value;
 	var password = document.getElementById("loginPassword").value;
 
@@ -16,4 +14,35 @@ function doLogin() {
 	req.open("POST", "/login");
 	req.setRequestHeader("Content-Type", "application/json");
 	req.send('{"login":"' + login + '","password":"' + password + '"}');
+}
+
+function createObr() {
+
+    // Запрос: {"id":uint32, "title":"TITLE", "content":"CONTENT", "public":0|1,  "state":"draft|sign|post","address":"ADDRESS"}
+
+
+	var id = 0;
+    var title = document.getElementById("title").value;
+    var content = document.getElementById("content").value;
+    var publ = document.getElementById("public").value;
+    var state = document.getElementById("state").value;
+    var address = document.getElementById("address").value;
+
+	var req = new XMLHttpRequest();
+    if (publ == "public"){
+        publ = 1;
+    } else {
+        publ = 0;
+    }
+    console.log(publ);
+	req.onload = function(e) {
+				window.location.replace("/");
+	};
+	req.onerror = function(e) {
+		document.getElementById("loginWarn").innerText = "Service temporarily unavailable.";
+	};
+
+	req.open("POST", "/json/obr");
+	req.setRequestHeader("Content-Type", "application/json");
+	req.send('{"id":"' + id + '","title":"' + title + '","title":"' + content + '","public":"' + publ + '", "state":"' + state + '", "address":"' + address + '"}');
 }
